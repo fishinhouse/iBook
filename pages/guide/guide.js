c1ab8224e1a7aebb4f0e560013929c15cd6cc58d
王靓 <fishinhouse@163.com>
2017-08-03 07:47:25 +0800

    add guide page

---
+++ pages/guide/guide.js
@@ -0,0 +1,33 @@
+Page({
+  data: {
+    imgUrls: [
+      '../../resources/1.jpg',
+      '../../resources/2.jpg',
+      '../../resources/3.jpg'
+    ],
+    indicatorDots: false,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000
+  },
+  changeIndicatorDots: function(e) {
+    this.setData({
+      indicatorDots: !this.data.indicatorDots
+    })
+  },
+  changeAutoplay: function(e) {
+    this.setData({
+      autoplay: !this.data.autoplay
+    })
+  },
+  intervalChange: function(e) {
+    this.setData({
+      interval: e.detail.value
+    })
+  },
+  durationChange: function(e) {
+    this.setData({
+      duration: e.detail.value
+    })
+  }
+})

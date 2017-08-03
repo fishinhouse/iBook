9bfe5639a4307511a2eb44678d279db81d638666
王靓 <fishinhouse@163.com>
2017-08-03 21:41:02 +0800

    add share button.

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,11 +4,12 @@ var app = getApp()
 Page({
   data: {
     motto: '',
+    gender:'',
     userInfo:{}
   },
   //事件处理函数
   bindViewTap: function() {
-    wx.navigateTo({
+    wx.switchTab({
       url: '../logs/logs'
     })
   },
@@ -19,8 +20,31 @@ Page({
     app.getUserInfo(function(userInfo){
       //更新数据
       that.setData({
-        userInfo:userInfo
+        userInfo:userInfo,
+        gender:userInfo.gender==0?'未知':userInfo.gender==1?'男':'女'
       })
     })
+    //显示当前页面的转发按钮
+    wx.showShareMenu({
+      withShareTicket: true
+    })
+  },
+  onShareAppMessage:function(res){
+    if (res.from === 'button') {
+      // 来自页面内转发按钮
+      console.log("==> 来自页面内转发按钮:"+res.target)
+    }
+    return {
+      title: '爱美文',
+      path: '/pages/index/index',
+      success: function(res) {
+        // 转发成功
+        console.log('==> 转发成功!')
+      },
+      fail: function(res) {
+        // 转发失败
+        console.log('==> 转发失败!')
+      }
+    }
   }
 })
